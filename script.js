f2b936a3d65da401614f998a7197df70d23610bc
Paul Foster <paulfosterdesign@outlook.com>
2021-01-06 16:48:57 +0000

    hourcycle 23h
    23h midnight displays as 00: not 24:

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ function buildOutput(generation) {
   
   let domDatainfo = document.createElement('p');
   domDatainfo.className = 'lowlight';
-  domDatainfo.textContent = `Updated ${new Intl.DateTimeFormat('en-GB', {month:"2-digit",year:"2-digit",day:"2-digit",hour:"2-digit",minute:"2-digit",hourCycle:"h24"}).format(new Date(griddata.to))}`;
+  domDatainfo.textContent = `Updated ${new Intl.DateTimeFormat('en-GB',{month:"2-digit",year:"2-digit",day:"2-digit",hour:"2-digit",minute:"2-digit",hourCycle:"h23"}).format(new Date(griddata.to))}`;
   
   resetdata();
   domList.dataset.timeto = DOMPurify.sanitize(griddata.to);
