b7bbbdb59de2a8ea7f8847e9935982f67a53941f
Paul Foster <paulfosterdesign@outlook.com>
2021-10-19 14:15:14 +0100

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    mode: 'production',
+    output: {
+        path: `${__dirname}/dist`,
+        filename: 'main.js',
+    },
+    plugins: [new MiniCssExtractPlugin()],
+    module: {
+    rules: [
+        {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+        },
+    ],
+    },
+};
