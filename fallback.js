dd68e66bdd60e06c7d1ca5200e717a906bd65c56
Paul Foster <paulfosterdesign@outlook.com>
2022-04-28 18:35:01 +0100

    Update fallback.js

--- fallback.js
+++ fallback.js
@@ -1,37 +1,44 @@
-const output = document.querySelector('#output');
-
 function buildOutput(generation) {
 
-  if(!output) return;
-  
-  let domList = document.createElement('ul');
-  for (let gensource of generation.data.generationmix) {
-    domList.innerHTML += `<li class="cover">${gensource.fuel} <span class="num">${gensource.perc}%</span></li>`;
-  };
-  
-  let domDatainfo = document.createElement('p');
-  domDatainfo.className = 'lowlight';
-  domDatainfo.textContent = `Updated ${new Date(generation.data.to).toLocaleString('en-GB')}`;
+  var domList = document.createElement('ul');
+  for (i = 0; i < generation.data.generationmix.length; i++) {
+    var gensource = generation.data.generationmix[i];
+    domList.innerHTML += '<li class="cover">' + gensource.fuel + '<span class="num">' + gensource.perc + '%</span></li>';
+  }
 
-  output.innerHTML = "";
-  document.querySelector('.loader').toggleAttribute('hidden');
-  output.append(domList, domDatainfo);
-    
+  var domDatainfo = document.createElement('p');
+  domDatainfo.className = 'lowlight';
+  domDatainfo.textContent = 'Updated ' + new Date(generation.data.to).toLocaleString('en-GB');
+
+  document.querySelector('#output').innerHTML = "";
+  document.querySelector('.loader').setAttribute('hidden', true);
+  document.querySelector('#output').append(domList, domDatainfo);
+
 }
 
-let fetchdata = async () => {
-  let response = await fetch(
-    'https://api.carbonintensity.org.uk/generation', {cache: 'reload'}
-  ).catch(error => {
-    output.textContent = `Sorry, error fetching grid data [${error}]`;
-  });
-  let data = await response.json();
-  try {
-    buildOutput(data);
-  } 
-  catch(error) {
-    output.textContent = `Sorry, error generating grid data (${error.name})`;
-  }
-};
+function fetchdata() {
+  fetch('https://api.carbonintensity.org.uk/generation', { cache: 'reload' }).then(
+    function (data) {
+      if (data.status === 200) {
+        return data.json();
+      }
+    }
+  ).then(
+    function (data) {
+      try {
+        buildOutput(data);
+      }
+      catch (error) {
+        document.querySelector('.loader').setAttribute('hidden', true);
+        document.querySelector('#output').textContent = 'Sorry, error generating grid data (' + error.name + ')';
+      }
+    }
+  ).catch(
+    function (error) {
+      document.querySelector('.loader').setAttribute('hidden', true);
+      document.querySelector('#output').textContent = 'Sorry, error fetching grid data (' + error.name + ')';
+    }
+  )
+}
 
 fetchdata();
