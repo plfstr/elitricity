3b250f2afeb1b7d3c381febde010c82df16c82ed
Paul <plfstr@users.noreply.github.com>
2024-11-28 12:36:32 +0000

    Meta theme color conditional

--- app.js
+++ app.js
@@ -7,21 +7,24 @@ if (window.trustedTypes && trustedTypes.createPolicy) {
 }
 
 // Set meta theme color to match intensity background...
-document.body.addEventListener("transitionend", () => {
-  let domMetacolor = document.querySelector('meta[name="theme-color"]');
-  // Create meta tag if none exists...
-  if (!domMetacolor) {
+const domMetacolor = document.querySelector('meta[name="theme-color"]');
+const dynamicthemecolor = !window.matchMedia('(min--moz-device-pixel-ratio: 0)').matches;
+if (!domMetacolor) {
     domMetacolor = document.createElement('meta');
     domMetacolor.name = "theme-color";
     document.head.append(domMetacolor);
-  }
-  // Apply color...
-  try {
-    domMetacolor.content = getComputedStyle(document.body).getPropertyValue("--col-background");
-  } catch (err) {
-    domMetacolor.content = "#fff";
-  };
-});
+}
+if (dynamicthemecolor) {
+    document.body.addEventListener("transitionend", () => {
+      try {
+        domMetacolor.content = getComputedStyle(document.body).getPropertyValue("--col-background");
+      } catch (err) {
+        domMetacolor.content = "#fff";
+      }
+    })
+} else {
+  domMetacolor.content = "#fff";
+}
 
 let output = document.querySelector("#output");
 let timevalid = 1000 * 60 * 33;
