c126d63af3ea37475b30aac9de325dee994a43d1
Paul <plfstr@users.noreply.github.com>
2025-08-08 17:52:09 +0100

    Region data

--- app.js
+++ app.js
@@ -36,6 +36,26 @@ export class GridSources extends LitElement {
     this.griddata = null;
     this.message = '';
     this.expired = false;
+    this.regions = {
+      "North Scotland": 1,
+      "South Scotland": 2,
+      "North West England": 3,
+      "North East England": 4,
+      "Yorkshire": 5,
+      "North Wales": 6,
+      "South Wales": 7,
+      "West Midlands": 8,
+      "East Midlands": 9,
+      "East England": 10,
+      "South West England": 11,
+      "South England": 12,
+      "London": 13,
+      "South East England": 14,
+      "England": 15,
+      "Scotland": 16,
+      "Wales": 17
+    },
+    this.regionsdata = '';
     this.addEventListener('refreshnow', (e) => e.detail.refresh ? this.fetchdata() : false);
     window.addEventListener("offline", (event) => {
       this.message = 'Currently offline. Displaying cached data.'
